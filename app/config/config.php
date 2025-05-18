3f93b45a974a07ed902ee8a928f63d2bc6e37cd2
A-Garcia0510 <agarcia2024@alu.uct.cl>
2025-05-18 12:25:15 -0400

    nose

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
 return [
     'app' => [
         'name' => 'Café-VT',
-        'url' => '//localhost/pruebas-ds/public/', // Añadido slash final', // Cambia esto según tu entorno
+        'url' => '', // Añadido slash final', // Cambia esto según tu entorno
         'env' => 'development', // 'development' o 'production'
     ],
     
